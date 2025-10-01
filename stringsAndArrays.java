7335e13c4ea8139dfcd994e852715137c83c976d
Tracy Phyron <tracy.jinor@ashesi.edu.gh>
2025-10-01 04:29:46 +0000

    checkpoint4

--- stringsAndArrays.java
+++ stringsAndArrays.java
@@ -4,7 +4,7 @@ public class stringsAndArrays {
         String txt1 = "my name is Hannah";// Hannah starts at index 11 counting form zero, that is what 'indexOf' does in java.
         System.out.println(txt1.indexOf("Hannah"));// single codes are for character data types , double codes are for strings.
         System.out.println(txt1.charAt(1)); //because character data type can only take one charater, we can easily use it to get the index of a single character.
-        String txt2 = "i am a dispenser of the Grace God";
+        String txt2 = "i am a dispenser of the God's grace";
         System.out.println(txt1.equals(txt2));// .equals() method compares the equality of two strings.
         System.out.println(txt1.trim());//removes leading and trailing widespaces.
          String txt3 = "  my name \\ is \"Hannah\"";
@@ -21,6 +21,8 @@ public class stringsAndArrays {
         
         int randomNumber = (int)(Math.random()*101);
         System.out.println(randomNumber);//also returns a double
+
+        //JAVA CONDITIONS AND IF STATEMENTS
         
         
 
